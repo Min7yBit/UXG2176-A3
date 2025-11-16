13331c0d633395b2d69e97e0ef4ac0ec89c197f0
kmmula69 <143325922+kmmula69@users.noreply.github.com>
2025-11-16 11:06:19 +0800

    Update
    - inventory UI done
    - pause menu UI done

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public GameObject inventory;
+
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePauseMenu();
+        }
+        if (Input.GetKeyDown(KeyCode.I) && !isPaused)
+        {
+            ToggleInventory();
+        }
+    }
+
+    public void TogglePauseMenu()
+    {
+        isPaused = !isPaused;
+        pauseMenu.SetActive(isPaused);
+        Time.timeScale = isPaused ? 0 : 1;
+        Cursor.lockState = isPaused ? CursorLockMode.None : CursorLockMode.Locked; //if paused, unlock cursor
+    }
+    public void ToggleInventory()
+    {        
+        inventory.SetActive(!inventory.activeSelf);
+        Cursor.lockState = inventory.activeSelf ? CursorLockMode.None : CursorLockMode.Locked; //if inventory open, unlock cursor
+    }
+}
