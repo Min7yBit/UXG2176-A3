07a5638166a3bf5bcf70d9b76104f3de76007211
kmmula69 <143325922+kmmula69@users.noreply.github.com>
2025-11-20 00:32:16 +0800

    Update
    - tray can be looked
    - spoon can be picked up

---
+++ Assets/Scripts/Interactions/Inteeractables/Tray.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Tray : MonoBehaviour, IInteractable
+
+{
+    public string Name => name;
+    public bool canInteract => interactable;
+
+    public CameraControl cameraControl;
+    public Camera cam;
+
+    private bool interactable = true;
+    private PlayerMovement playerMovement;
+
+    public Transform GetTransform()
+    {
+        return transform;
+    }
+
+    public void OnInteract(in PlayerMovement playerMovement)
+    {
+        interactable = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        this.playerMovement = playerMovement;
+        this.playerMovement.CanMove = false;
+        cameraControl.SwitchToFixedCamera(cam);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            interactable = true;
+            playerMovement.CanMove = true;
+            cameraControl.SetCameraMode(CameraControl.CameraMode.ThirdPerson);
+
+        }
+    }
+
+}
